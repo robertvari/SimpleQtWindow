c144b6233e1dedb5e63e23f091284786b94d32b1
Robert Vari <mail.pythonsuli@gmail.com>
2021-07-24 17:04:35 +0200

    window refactored

--- main.py
+++ main.py
@@ -22,6 +22,16 @@ class RegistrationForm(QWidget):
         self.last_name_field.setPlaceholderText("Last name")
         main_layout.addWidget(self.last_name_field)
 
+        save_button = QPushButton("Save")
+        main_layout.addWidget(save_button)
+
+        # connect signals/slot mechanism
+        save_button.clicked.connect(self.save_action)
+
+    def save_action(self):
+        print(f"First name: {self.first_name_field.text()}")
+        print(f"Last name: {self.last_name_field.text()}")
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
