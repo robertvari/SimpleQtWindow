9df8899704b58c72aad9d21f386d8483ba9738dd
Robert Vari <mail.pythonsuli@gmail.com>
2021-07-24 16:38:36 +0200

    window refactored

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from PySide6.QtWidgets import QWidget, QApplication
+from PySide6.QtWidgets import QWidget, QApplication, QLineEdit, \
+    QPushButton, QVBoxLayout
 import sys
 
 
@@ -6,9 +7,15 @@ class RegistrationForm(QWidget):
     def __init__(self):
         super().__init__()
 
+        # window config
         self.setWindowTitle("Registration Form")
         self.resize(400, 600)
 
+        # window content (widgets)
+        main_layout = QVBoxLayout(self)
+
+        hello_button = QPushButton("Say Hello!")
+        main_layout.addWidget(hello_button)
 
 
 if __name__ == '__main__':
